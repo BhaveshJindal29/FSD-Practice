1e8dd369475011faf3dbffbb261ef6628cffd538
Bhavesh Jindal <63338442+BhaveshJindal29@users.noreply.github.com>
2022-03-26 15:34:03 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,22 @@
+const numbers = [1,4,8,9];
+
+
+var maximum = numbers[0];
+var minimum = numbers[0];
+
+for (let i = 0; i < numbers.length; ++i) {
+  if (numbers[i] > maximum) {
+    maximum = numbers[i];
+  }
+
+}
+
+  for (let i = 0; i < numbers.length; ++i) {
+    if (numbers[i] < minimum) {
+        minimum = numbers[i];
+    }
+}
+
+            
+console.log(minimum);
+console.log(maximum);
